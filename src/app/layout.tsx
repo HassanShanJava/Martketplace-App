cfb5e294036930b9e3858f76459a32289500739b
HassanShanJava <hassanshan675@gmail.com>
2023-07-03 01:47:59 +0500

    added responsiveness to hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
+        <main className="px-8">
+
         {children}
+        </main>
       </body>
     </html>
   );
--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -10,9 +10,10 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col lg:flex-row gap-y-10 py-6 mx-8 ">
+    <section className="flex flex-col lg:flex-row gap-y-10 py-6  ">
       {/* Left Div */}
       <div className="flex-1 ">
+        {/* flex-1 for equall width in flex parent */}
         <Badge className="py-2 px-6 text-lg rounded-lg bg-blue-200 text-blue-700 ">
           Sale 70%
         </Badge>
@@ -27,11 +28,22 @@ const Hero = () => {
       </div>
 
       {/* Right Div */}
-      <div className="flex-1 ">
-        <Image src={HeroImg} alt="hero" className="object-cover" />
+      <div className="flex-1 relative items-center  ">
+        {/* <div className="flex justify-between -z-10 items-center absolute  ">
+          <div className="  bg-orange-200  rounded-full w-[450px] h-[450px] "></div>
+        </div> */}
+        
+        <div className="relative flex h-[300px] sm:h-[460px] w-fit items-center justify-center mx-auto  before:absolute before:h-[260px] before:w-[260px]  sm:before:h-[420px] sm:before:w-[420px] before:rounded-full before:bg-orange-200">
+          <Image
+            src={HeroImg}
+            alt="hero"
+            className=" z-30 w-[300px] h-[300px] sm:w-[460px] sm:h-[460px]  m-auto"
+          />
+        </div>
       </div>
     </section>
   );
 };
 
 export default Hero;
+1;
