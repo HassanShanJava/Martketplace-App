8bcb67f4126f1fe376a9ed70969e250176a447e2
HassanShanJava <hassanshan675@gmail.com>
2023-07-03 02:15:27 +0500

    added product info

--- src/views/ProductList.tsx
+++ src/views/ProductList.tsx
@@ -7,7 +7,7 @@ const ProductList = () => {
     <section>
       <div className="flex justify-around gap-3 mt-16">
         <ProductCard title="abc" price={100} img={P1} />
-        <ProductCard title="abc" price={100} img={P1} />
+        <ProductCard title="awsd" price={130} img={P1} />
         <ProductCard title="xyz" price={100} img={P1} />
         <ProductCard title="jacket" price={1000} img={P1} />
       </div>
