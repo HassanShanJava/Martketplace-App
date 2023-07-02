2308498fa011741590ed4daabfe8f2c55a45d948
HassanShanJava <hassanshan675@gmail.com>
2023-07-03 02:12:57 +0500

    added prodult list data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "@/components/layout/Header";
 import Hero from "@/views/Hero";
+import ProductList from "@/views/ProductList";
 
 export default function Home() {
   return (
     <>
       <div>
         <Hero />
+        <ProductList/>
       </div>
     </>
   );
